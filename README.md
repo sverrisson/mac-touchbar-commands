e629ebe76706a33531e4e7f8aabf9fa5650293f5
Hannes Sverrisson <tinni@Tinni.local>
2017-11-05 20:02:21 +0000

    Experiments

---
+++ README.md
@@ -0,0 +1,70 @@
+# Mac Touch Bar Commands README
+
+Touch Bar commands for easy and fast access.
+
+## Features
+
+Touch Bar commands for easy and fast access. 
+Term -> Toggle the built in terminal on/off.
+// -> Toggle comments on selected lines on/off.
+Qopen -> Quick Open any file etc.
+Commands -> Open the command pallette.
+Preview -> Toggle Markdown Preview
+
+\!\[Touch Bar with commands\]\(images/Touch Bar Commands.png\)
+
+## Requirements
+
+A computer or keyboard with Touch Bar is required.
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+Added features X, Y, and Z.
+
+## Credit
+Thanks to:
+Icon touch bar by mikicon from the Noun Project
+
+-----------------------------------------------------------------------------------------------------------
+
+## Working with Markdown
+
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,63 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and commanderence it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    let touchbar = [
+        {
+            'command': 'extension.TouchBar.Term',
+            'action': 'workbench.action.terminal.toggleTerminal'
+        },
+        {
+            'command': 'extension.TouchBar.Comment',
+            'action': 'editor.action.commentLine'
+        },
+        {
+            'command': 'extension.TouchBar.Qopen',
+            'action': 'workbench.action.quickOpen'
+        },
+        {
+            'command': 'extension.TouchBar.Commands',
+            'action': 'workbench.action.showCommands'
+        },
+        {
+            'command': 'markdown.showPreview',
+            'action': 'markdown.showPreview'
+        }
+    ]
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Commands: ' + touchbar.length);
+    //commands.forEach((cmd) => console.log(cmd.name));
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    touchbar.forEach(
+        (cmd) => {
+            console.log("skipun: " + cmd.command);
+            let command = vscode.commands.registerCommand(cmd.command, 
+                () => {
+                vscode.commands.executeCommand(cmd.action)
+            });
+            context.subscriptions.push(command);
+        });
+
+    // let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    //     // The code you place here will be executed every time your command is executed
+
+    //     // Display a message box to the user
+    //     vscode.window.showInformationMessage('Hello World!');
+    // });
+
+    // context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
